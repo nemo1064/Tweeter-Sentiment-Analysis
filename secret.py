08d49d7b506d9b4e8c501b7aa5e62ed6d8557603
nemo1064 <47845106+nemo1064@users.noreply.github.com>
2019-11-13 15:28:10 +0530

    Update secret.py

--- secret.py
+++ secret.py
@@ -1,5 +1,5 @@
-consumer_key = 'heuaCDZJKfqi06JMB6aGGztSi'
-consumer_secret = 'JMJJ3JMzCzjMqXgE1i75x9rEMTIIFtSDoZEMVqTWTgzZh2Jn31'
-access_token = '1027585656896147458-gNOwPZX0p9KKr9sFQYJoCwgSlcssh2'
-access_token_secret = '4GTxAn8UelzH9btznHM0fdP2riuFlfsPV9jDRZfNoJIyD'
+consumer_key = '****************************'
+consumer_secret = '****************************'
+access_token = '***************************'
+access_token_secret = '**********************************'
 
